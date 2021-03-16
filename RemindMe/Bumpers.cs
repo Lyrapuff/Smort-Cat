928c9093d995fb9273c34bdd3f3eabbd0889556d
Lyrapuff <lyrapuff@gmail.com>
2021-03-16 23:56:56 +0300

    Hardcoded bump reminders

---
+++ RemindMe/Bumpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace RemindMe
+{
+    public class Bumpers
+    {
+        private SocketTextChannel _channel;
+
+        public Bumpers(DiscordSocketClient client)
+        {
+            SocketGuild guild = client.GetGuild(808725004367822920);
+            _channel = guild.GetTextChannel(808815737674006578);
+        }
+        
+        public async ValueTask Start()
+        {
+            while (true)
+            {
+                // Wait for 2 hours
+                await Task.Delay(1000 * 60 * 60 * 2);
+                await _channel.SendMessageAsync($"<@&821486024898773033>{Environment.NewLine}Bump! uwu");
+            }
+        }
+    }
+}
---
+++ RemindMe/Module.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using SmortCat.Domain.Modules;
+
+namespace RemindMe
+{
+    public class Module : IModule
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<Bumpers>();
+        }
+
+        public void Start(IServiceProvider provider)
+        {
+            Task.Run(provider.GetService<Bumpers>().Start);
+        }
+    }
+}
